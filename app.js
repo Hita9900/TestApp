805d9bceb2ac173125cf4e3eee270381e93eceb2
Anahita N <anahita.nazari.99@gmail.com>
2025-06-29 17:12:49 +0330

    debugging

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const supabaseUrl = 'https://zxoejcljodfirstxadkl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4b2VqY2xqb2RmaXJzdHhhZGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExNDAwMTUsImV4cCI6MjA2NjcxNjAxNX0.s-YFL42yB5smxiWTBDzNUZX8ZQ861wy6DrLjoPwfpvo';
 // Initialize Supabase
-console.log("test 2: Supabase is:", typeof supabase); 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 console.log("test 2: Supabase is:", typeof supabase); 
 
