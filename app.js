d9c06e01e681f17029006d70dc9709ac38864143
Anahita N <anahita.nazari.99@gmail.com>
2025-06-29 22:37:26 +0330

    Small tweaks (to fix data submitting)

--- app.js
+++ app.js
@@ -64,6 +64,44 @@ export async function saveData(title, content) {
     }
 }
 
+// Load data when dashboard opens
+document.addEventListener('DOMContentLoaded', async () => {
+const data = await fetchUserData();
+const dataList = document.getElementById('data-list');
+        
+if (data.length === 0) {
+    dataList.innerHTML = '<p>No data saved yet.</p>';
+    } else {
+        let html = '';
+        data.forEach(item => {
+            html += `
+                <div class="data-item">
+                    <h3>${item.title}</h3>
+                    <p>${item.content}</p>
+                </div>
+                `;
+            });
+        dataList.innerHTML = html;
+    }
+});
+
+
+//fetchUserData
+async function fetchUserData() {
+    const user = supabaseClient.auth.user();
+    
+    const { data, error } = await supabaseClient
+        .from('user_data')
+        .select('*')
+        .eq('user_id', user.id);
+    
+    if (error) {
+        console.error('Error fetching data:', error);
+    } else {
+        console.log('User data:', data);
+        return data;
+    }
+}
 /*
 //service worker registration
 if ('serviceWorker' in navigator) {
